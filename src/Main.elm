34db894c9fcc2a9a6bef562b51d83da6046ce727
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 19:07:26 +0530

    minimal elm todoist clone

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Browser
 import Emoji
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -11,7 +12,15 @@ todoList =
     [ "Get Milk", "Remember to call", "Do Stuff!", "And More" ]
 
 
-main =
+init _ =
+    ( (), Cmd.none )
+
+
+update _ _ =
+    ( (), Cmd.none )
+
+
+view _ =
     div []
         [ UI.topBar [ UI.search, UI.filler, UI.addIconBtn ]
         , main_ [ class "measure center" ]
@@ -30,3 +39,8 @@ viewTodoListItem title =
 
 todoListLi =
     li [ class "lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" ]
+
+
+main : Program () () ()
+main =
+    Browser.element { init = init, view = view, update = update, subscriptions = \_ -> Sub.none }
