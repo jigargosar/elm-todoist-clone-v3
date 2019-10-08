5beb8cff43f7aea39ff502189396a12b92016535
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 23:57:11 +0530

    minimal elm todoist clone

--- src/Main.elm
+++ src/Main.elm
@@ -90,6 +90,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ Appbar.view
+        , div [ style "height" "2.5rem" ] []
         , main_ [ class "measure center" ]
             [ Todo.viewList { toggle = Toggle } (todoList model)
             ]
