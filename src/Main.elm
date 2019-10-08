17ff56be1ed227eea781f62c8b8b636ab617960b
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 18:23:21 +0530

    minimal elm todoist clone

--- src/Main.elm
+++ src/Main.elm
@@ -9,30 +9,10 @@ todoList =
     [ "Get Milk", "Remember to call", "Do Stuff!", "And More" ]
 
 
-foo =
-    onClick
-
-
-buttonClass =
-    """
-    f6 f5-ns fw6 dib
-    ba br-pill b--black-20
-    bg-blue white
-    ph3 ph4-ns pv2 pv3-ns
-    grow no-underline
-    """
-
-
 main =
     div []
         [ header [ class "pa4" ]
-            [ button
-                [ class buttonClass, class "mr2" ]
-                [ text <| String.fromChar heavy_plus_sign ]
-            , button
-                [ class buttonClass, class "mr2" ]
-                [ text "+" ]
-            ]
+            []
         , main_ []
             [ ol [ class "list pl0 measure center" ]
                 (List.map viewTodoListItem todoList)
