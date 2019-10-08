6bc86ae329af7d312a3d76baebd0acd8d56b888a
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 21:48:02 +0530

    minimal elm todoist clone

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+
+  devServer: {
+    contentBase: 'public',
+    watchContentBase: true,
+  },
 }
