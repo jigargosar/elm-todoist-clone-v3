d1bedf3e4160ad9366a52f0287eee496be362097
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 15:42:24 +0530

    minimal elm todoist clone

--- src/Main.elm
+++ src/Main.elm
@@ -3,5 +3,14 @@ module Main exposing (..)
 import Html exposing (..)
 
 
+todoList =
+    [ "Get Milk", "Remember to call", "Do Stuff!" ]
+
+
 main =
-    text "Hello"
+    div []
+        (List.map viewTodoListItem todoList)
+
+
+viewTodoListItem title =
+    div [] [ text title ]
