81869797942c7c2413ae3da90e43a3eb8d3b6bab
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-08 22:51:36 +0530

    minimal elm todoist clone

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,12 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
   module: {
     rules: [
-      { include: /\.elm/, use: ['elm-hot-webpack-loader', 'elm-webpack-loader'] },
-      { include: /\.css/, use: ['style-loader', 'css-loader'] }
-      ],
+      {
+        include: /\.elm/,
+        use: ['elm-hot-webpack-loader', 'elm-webpack-loader'],
+      },
+      { include: /\.css/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   devServer: {
     contentBase: 'public',
