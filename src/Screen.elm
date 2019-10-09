c4c81500eca374b4d85e55af3aa594b9cce942a2
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-09 17:12:05 +0530

    minimal elm todoist clone

--- src/Screen.elm
+++ src/Screen.elm
@@ -65,23 +65,23 @@ subscriptions _ =
     Sub.batch [ onResize Changed ]
 
 
-view h s m =
+view top side main =
     div [ class "bg-body" ]
         [ header [ class "fixed top-0 bg-light-red white w-100 h-header" ]
             [ div
                 ([ class "center w-100 max-w-app ph2" ]
                     ++ [ class "h-100", class "flex items-center" ]
                 )
-                h
+                top
             ]
         , div [ class "center w-100 max-w-app ", class "flex-grow-1" ]
             [ aside
                 [ class "dn db-ns fixed top-sidebar bottom-0 w-sidebar hover-overflow-y  br-ns b--main"
                 ]
-                s
+                side
             , div
                 [ class "ml0 ml-main-ns pt-main min-vh-100 flex-grow-1 flex"
                 ]
-                [ main_ [ class "flex-grow-1 bg-white br-ns b--main" ] m ]
+                [ main_ [ class "flex-grow-1 bg-white br-ns b--main" ] main ]
             ]
         ]
