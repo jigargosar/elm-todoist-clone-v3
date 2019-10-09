ba6d5daf25fbdfc6246f0f50d6d19659a860c1a0
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-09 18:35:58 +0530

    minimal elm todoist clone

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
+import ports from './ports'
+
 const Module = require('./Main.elm')
 require('tachyons')
 require('./style.css')
 
-Module.Elm.Main.init({
+const app = Module.Elm.Main.init({
   node: document.getElementById('root'),
 })
 
-// const W = Worker
-const W = window['SharedWorker']
-console.debug()
-const w = new W('worker.js', { name: 'worker' })
-
-console.log(w)
-
-w.port.postMessage('hw')
-
-w.port.onmessage = function(e) {
-  console.log(e)
-}
+ports([], {}, app)
---
+++ src/ports.js
@@ -0,0 +1,36 @@
+import { isEmpty, mapObjIndexed, omit, pathOr } from 'ramda'
+
+export default function ports(pubNames = [], subs = {}, app) {
+  const pubs = pubNames.map(name => {
+    const send = pathOr(null, ['ports', name, 'send'])
+    if (!send) {
+      console.warn('Elm Sub Port Not Found', name)
+    }
+    return function(payload) {
+      if (!send) {
+        console.warn('Elm Sub Port Not Found. Send failed', {
+          name,
+          payload,
+        })
+      } else {
+        send(payload)
+      }
+    }
+  })
+
+  mapObjIndexed((fn, subName) => {
+    const subscribe = pathOr(null, ['ports', subName, 'subscribe'])
+    if (!subscribe) {
+      console.warn('Elm Cmd port Not Found', subName)
+    } else {
+      subscribe(payload => fn(payload, pubs))
+    }
+  })(subs)
+
+  const unhandledPorts = omit(Object.keys(subs) + pubNames, app.ports)
+  const unhandledPortNames = Object.keys(unhandledPorts)
+  if (!isEmpty(unhandledPortNames)) {
+    console.warn('unhandledPorts', unhandledPortNames)
+  }
+  return pubs
+}
