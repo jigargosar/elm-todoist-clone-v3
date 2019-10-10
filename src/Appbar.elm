3c12b4ce970146f1d94083496505ef02e7950bce
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-10 19:58:31 +0530

    minimal elm todoist clone

--- src/Appbar.elm
+++ src/Appbar.elm
@@ -2,12 +2,14 @@ module Appbar exposing (view)
 
 import Html.Styled exposing (..)
 import MaterialIcons
+import Styles exposing (ph)
 import UI
 
 
 view : List (Html msg)
 view =
     [ UI.menuIconBtn
+    , styled div [ ph 1 ] [] []
     , UI.search
     , UI.filler
     , UI.addIconBtn
