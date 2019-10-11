aae5fedcf34862d3b58f3fc20c8916ce597eb4cc
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-11 18:26:36 +0530

    minimal elm todoist clone

--- src/Drawer.elm
+++ src/Drawer.elm
@@ -1,6 +1,7 @@
 module Drawer exposing (view)
 
 import Css
+import Css.Transitions as Transitions exposing (transition)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css)
 import MaterialIcons as MI
@@ -31,16 +32,25 @@ navItem title =
     styled div [ pa 2, pointer ] [] [ text title ]
 
 
-expansionPanel isExpanded title content =
-    div [] (expansionPanelHeader title :: content)
+expansionPanel collapsed title content =
+    div [] (expansionPanelHeader collapsed title :: content)
 
 
-expansionPanelHeader title =
+expansionPanelHeader collapsed title =
     div
         [ css [ pa 1, bo_b, boc (grayL 0.9), flex, hover [ bgGrayL 0.95 ] ] ]
         [ button
             [ css [ iBtnStyle, flexGrow1 ] ]
-            [ span [ css [ c_grayL 0.6 ] ] [ MI.expand_more ]
+            [ span
+                [ css
+                    [ c_grayL 0.6
+                    , batch
+                        [ styleIf collapsed [ Css.transforms [ Css.rotate (Css.deg 90) ] ]
+                        , transition [ Transitions.transform 200 ]
+                        ]
+                    ]
+                ]
+                [ MI.expand_more ]
             , styled span [ bold, pa 1 ] [] [ text title ]
             ]
         , button [ css [ iBtnStyle ] ] [ MI.add ]
