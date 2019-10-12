4cc0dd91b5c916ae12c9de0ed3a1ae87319c9cd7
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-12 12:11:06 +0530

    minimal elm todoist clone

--- src/ExpansionPanel.elm
+++ src/ExpansionPanel.elm
@@ -1,4 +1,12 @@
-module ExpansionPanel exposing (ExpansionPanel, initial, update)
+module ExpansionPanel exposing (ExpansionPanel, initial, update, view, viewHeader)
+
+import Css
+import Css.Transitions as Transitions exposing (transition)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick)
+import MaterialIcons as MI
+import Styles exposing (..)
 
 
 type ExpansionPanel
@@ -13,14 +21,21 @@ initial =
     Internal False |> ExpansionPanel
 
 
+collapsed : ExpansionPanel -> Bool
+collapsed =
+    unwrap >> .collapsed
+
+
 type Msg
     = Toggle
 
 
+unwrap : ExpansionPanel -> Internal
 unwrap (ExpansionPanel internal) =
     internal
 
 
+map : (Internal -> Internal) -> ExpansionPanel -> ExpansionPanel
 map func =
     unwrap >> func >> ExpansionPanel
 
@@ -30,3 +45,49 @@ update toMsg message model =
     case message of
         Toggle ->
             ( map (\i -> { i | collapsed = not i.collapsed }) model, Cmd.none )
+
+
+iBtnStyle =
+    batch [ btnReset, pointer ]
+
+
+viewHeader : (Msg -> msg) -> String -> ExpansionPanel -> Html msg
+viewHeader toMsg title model =
+    let
+        isCollapsed =
+            collapsed model
+    in
+    div
+        [ css [ bo_b, boc (grayL 0.9), flex, hover [ bgGrayL 0.95 ] ] ]
+        [ button
+            [ css [ iBtnStyle, pa 1, flexGrow1 ], onClick (toMsg Toggle) ]
+            [ span
+                [ css
+                    [ c_grayL 0.6
+                    , batch
+                        [ styleIf isCollapsed [ Css.transforms [ Css.rotate (Css.deg -90) ] ]
+                        , transition [ Transitions.transform 200 ]
+                        ]
+                    ]
+                ]
+                [ MI.expand_more ]
+            , styled span [ bold, pa 1 ] [] [ text title ]
+            ]
+        , button [ css [ iBtnStyle ] ] [ MI.add ]
+        ]
+
+
+view : Html msg -> List (Html msg) -> ExpansionPanel -> Html msg
+view headerHtml contentHtml model =
+    let
+        isCollapsed =
+            collapsed model
+
+        finalContent =
+            if isCollapsed then
+                []
+
+            else
+                contentHtml
+    in
+    div [] (headerHtml :: contentHtml)
