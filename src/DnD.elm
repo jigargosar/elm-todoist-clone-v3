9cd3f2b5c185048a6ba6b8ab338a8c2926e8f2f0
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-14 18:19:07 +0530

    minimal elm todoist clone

--- src/DnD.elm
+++ src/DnD.elm
@@ -1,6 +1,7 @@
-module DnD exposing (..)
+module DnD exposing (DnD)
 
 import Browser.Dom as Dom
+import Lens
 
 
 type DnD
@@ -16,8 +17,8 @@ type alias Position =
 
 
 type alias State =
-    { dragIndex : Int
-    , dropIndex : Int
+    { startPosition : Position
+    , currentPosition : Position
     , dragElement : Maybe Dom.Element
     , dropElement : Maybe Dom.Element
     , dragElementId : String
@@ -25,5 +26,65 @@ type alias State =
     }
 
 
+unwrap : DnD -> Internal
+unwrap (DnD internal) =
+    internal
+
+
+map : (Internal -> Internal) -> DnD -> DnD
+map func =
+    unwrap >> func >> DnD
+
+
+mapState : (State -> State) -> DnD -> DnD
+mapState func =
+    map (Maybe.map func)
+
+
+initial : DnD
 initial =
     DnD Nothing
+
+
+type alias ElementResult =
+    Result Dom.Error Dom.Element
+
+
+type Msg
+    = DragStart String Position
+    | Drag Position
+    | DragOver String
+    | GotDragElement ElementResult
+    | GotDropElement ElementResult
+
+
+update message model =
+    case message of
+        DragStart dragElementId xy ->
+            { startPosition = xy
+            , currentPosition = xy
+            , dragElementId = dragElementId
+            , dropElementId = dragElementId
+            , dragElement = Nothing
+            , dropElement = Nothing
+            }
+                |> Just
+                |> DnD
+
+        Drag xy ->
+            mapState (\s -> { s | currentPosition = xy }) model
+
+        DragOver dropElementId ->
+            mapState (\s -> { s | dropElementId = dropElementId }) model
+
+        GotDragElement (Err _) ->
+            model
+
+        GotDragElement (Ok dragElement) ->
+            mapState (\s -> { s | dragElement = Just dragElement, dropElement = Just dragElement }) model
+
+        GotDropElement (Err _) ->
+            model
+
+        GotDropElement (Ok dropElement) ->
+            mapState (\s -> { s | dropElement = Just dropElement }) model
