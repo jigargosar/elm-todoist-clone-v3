344d4c05307c96ea4fda8f0be9d81e165337c294
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-18 12:01:44 +0530

    minimal elm todoist clone

--- src/XYDelta.elm
+++ src/XYDelta.elm
@@ -8,18 +8,6 @@ type XYDelta
     = MouseMoveDelta XY XY
 
 
-pageXYDecoder : JD.Decoder XY
-pageXYDecoder =
-    JD.map2 XY
-        (JD.field "pageX" JD.float)
-        (JD.field "pageY" JD.float)
-
-
-fromPageXYDecoder : JD.Decoder XYDelta
-fromPageXYDecoder =
-    JD.map (\xy -> MouseMoveDelta xy xy) pageXYDecoder
-
-
 moveTo : XY -> XYDelta -> XYDelta
 moveTo xy (MouseMoveDelta start _) =
     MouseMoveDelta start xy
