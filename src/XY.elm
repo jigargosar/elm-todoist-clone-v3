1b3115c380050f9aab24938ce3b5540a37780ba0
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-18 11:37:48 +0530

    minimal elm todoist clone

---
+++ src/XY.elm
@@ -0,0 +1,19 @@
+module XY exposing (..)
+
+
+type alias XY =
+    { x : Float, y : Float }
+
+
+type alias HasXY a =
+    { a | x : Float, y : Float }
+
+
+subtract : HasXY a -> HasXY b -> XY
+subtract a b =
+    XY (a.x - b.x) (a.y - b.y)
+
+
+add : HasXY a -> HasXY b -> XY
+add a b =
+    XY (a.x + b.x) (a.y + b.y)
