9d4b6853b04b9d051e8576267e959f37ba0dd68e
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-19 15:59:04 +0530

    minimal elm todoist clone

--- src/StyleAttrs.elm
+++ src/StyleAttrs.elm
@@ -1,7 +1,8 @@
-module StyleAttrs exposing (StyleAttrs, concat)
+module StyleAttrs exposing (StyleAttrs, concat, toAttrs)
 
 import Css exposing (Style)
 import Html.Styled exposing (Attribute)
+import Html.Styled.Attributes exposing (css)
 
 
 type alias StyleAttrs msg =
@@ -11,3 +12,8 @@ type alias StyleAttrs msg =
 concat : StyleAttrs msg -> StyleAttrs msg -> StyleAttrs msg
 concat sa1 sa2 =
     StyleAttrs (sa1.styles ++ sa2.styles) (sa1.attrs ++ sa2.attrs)
+
+
+toAttrs : StyleAttrs msg -> List (Attribute msg)
+toAttrs { styles, attrs } =
+    css styles :: attrs
