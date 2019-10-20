757c2560110224d06158832ac2a3585dd8990b31
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-20 14:13:38 +0530

    minimal elm todoist clone

--- src/DrawerItem.elm
+++ src/DrawerItem.elm
@@ -1,12 +1,14 @@
 module DrawerItem exposing (..)
 
 import Html.Styled exposing (div)
+import Html.Styled.Attributes exposing (css)
 import StyleAttrs as SA
 import Styles exposing (..)
 
 
-view rootSA =
-    div (SA.toAttrsWithBase baseRootStyles [] rootSA) []
+view rootSA children =
+    div (SA.toAttrsWithBase baseRootStyles [] rootSA)
+        (children ++ [ div [ css [ mr 3 ] ] [] ])
 
 
 baseRootStyles =
