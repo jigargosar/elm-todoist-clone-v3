2abb1381ba3b9971e585a654b1ec901277a90cd8
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-27 17:45:59 +0530

    minimal elm todoist clone

--- src/ProjectPanel.elm
+++ src/ProjectPanel.elm
@@ -217,7 +217,7 @@ viewItem { itemAttrs, itemStyles, handleAttrs } project =
                 :: handleAttrs
             )
             [ text "folder" ]
-        , a [ css [ Px.p2 8 4, lh 1.5, flexGrow1 ], href ] [ text title ]
+        , a [ css [ linkReset, Px.p2 8 4, lh 1.5, flexGrow1 ], href ] [ text title ]
         , button
             [ css [ btnReset, pointer, Px.pa 4, Px.m2 4 0, flex, itemsCenter, selfEnd ]
             , class "show_on_parent_hover"
