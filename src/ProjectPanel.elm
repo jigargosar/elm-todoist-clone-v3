389f9f4bed6e3450cf01de2fcc33a85a6cf31e6d
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-27 17:34:42 +0530

    minimal elm todoist clone

--- src/ProjectPanel.elm
+++ src/ProjectPanel.elm
@@ -10,9 +10,7 @@ module ProjectPanel exposing
     )
 
 import Css
-import Css.Global
 import DNDList
-import DrawerUI
 import Html.Styled as H exposing (Attribute, Html, button, div, i, span, text)
 import Html.Styled.Attributes as A exposing (class, css)
 import Html.Styled.Events exposing (onClick)
@@ -142,12 +140,65 @@ viewExpanded =
 
 viewHeader : Bool -> List (Html Msg)
 viewHeader isExpanded =
-    DrawerUI.viewExpansionPanelHeader
+    viewExpansionPanelHeader
         { toggle = HeaderClicked
         , title = "Projects"
         , isExpanded = isExpanded
         , secondary = Just { iconName = "add", action = AddClicked }
         }
+
+
+viewExpansionPanelHeader :
+    { toggle : msg
+    , title : String
+    , isExpanded : Bool
+    , secondary : Maybe { iconName : String, action : msg }
+    }
+    -> List (Html msg)
+viewExpansionPanelHeader { toggle, isExpanded, title, secondary } =
+    let
+        expansionToggleBtn : Html msg
+        expansionToggleBtn =
+            let
+                iconName =
+                    if isExpanded then
+                        "expand_more"
+
+                    else
+                        "chevron_right"
+            in
+            button
+                [ css [ btnReset, pointer, flexGrow1, flex, itemsCenter, tal ], onClick toggle ]
+                [ i [ css [ Px.pa 4 ], class "material-icons" ] [ text iconName ]
+                , span [ css [ Px.p2 8 4, bold, flexGrow1 ] ] [ text title ]
+                ]
+
+        secondaryActionIconBtn : { a | iconName : String, action : msg } -> Html msg
+        secondaryActionIconBtn { iconName, action } =
+            button
+                [ css [ secondaryActionIconBtnStyle ]
+                , onClick action
+                ]
+                [ secondaryActionIcon iconName ]
+
+        secondaryActionIconBtnStyle : Style
+        secondaryActionIconBtnStyle =
+            batch [ btnReset, pointer, Px.pa 4, Px.m2 4 0, flex, itemsCenter, selfEnd ]
+
+        secondaryActionIcon : String -> Html msg
+        secondaryActionIcon name =
+            i [ class "material-icons" ] [ text name ]
+
+        listItemStyle : Style
+        listItemStyle =
+            batch [ Px.pl 4, Px.pr (4 + 16), flex, itemsCenter, bo_b, boc Theme.borderGray, hover [ bgGrayL 0.95 ] ]
+    in
+    [ div
+        [ css [ listItemStyle ] ]
+        [ expansionToggleBtn
+        , Maybe.map secondaryActionIconBtn secondary |> Maybe.withDefault (text "")
+        ]
+    ]
 
 
 secondaryActionMoreIconBtn =
