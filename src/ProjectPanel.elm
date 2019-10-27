ebb8249bc54a91c84510e54ea3c525ee450a4e1e
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-27 16:54:06 +0530

    minimal elm todoist clone

--- src/ProjectPanel.elm
+++ src/ProjectPanel.elm
@@ -156,10 +156,7 @@ viewHeader isExpanded =
             [ i [ css [ Px.pa 4 ], class "material-icons" ] [ text iconName ]
             , span [ css [ Px.p2 8 4, bold, flexGrow1 ] ] [ text "Projects" ]
             ]
-        , button
-            [ css [ btnReset, pointer, Px.pa 4, Px.m2 4 0, flex, itemsCenter, selfEnd ]
-            ]
-            [ i [ class "material-icons" ] [ text "add" ] ]
+        , secondaryActionAddIconBtn
         ]
     ]
 
@@ -197,12 +194,7 @@ viewItem { itemAttrs, itemStyles, handleAttrs } project =
             )
             [ text "folder" ]
         , div [ css [ Px.p2 8 4, lh 1.5, flexGrow1 ] ] [ text title ]
-        , button
-            [ css [ btnReset, pointer, Px.pa 4, Px.m2 4 0, flex, itemsCenter, selfEnd ]
-            , class "show_on_parent_hover"
-            ]
-            [ i [ class "material-icons" ] [ text "more_horiz" ]
-            ]
+        , secondaryActionMoreIconBtn
         ]
 
 
