42cc24fb5e508bf4f6112978be344acb3ac137c3
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-27 17:05:56 +0530

    minimal elm todoist clone

--- src/DrawerUI.elm
+++ src/DrawerUI.elm
@@ -1,4 +1,4 @@
-module DrawerUI exposing (..)
+module DrawerUI exposing (viewExpansionPanelHeader)
 
 import Html.Styled exposing (Attribute, Html, button, div, i, span, text)
 import Html.Styled.Attributes exposing (class, css)
@@ -8,8 +8,16 @@ import Styles exposing (..)
 import Theme
 
 
-viewExpansionPanelHeader : { toggle : msg, title : String, isExpanded : Bool } -> List (Html msg)
-viewExpansionPanelHeader { toggle, isExpanded, title } =
+viewExpansionPanelHeader :
+    { toggle : msg
+    , title : String
+    , isExpanded : Bool
+    , secondary : Maybe { iconName : String, action : msg }
+    , secondaryIcon : String
+    , secondaryAction : msg
+    }
+    -> List (Html msg)
+viewExpansionPanelHeader { toggle, isExpanded, title, secondary } =
     let
         iconName =
             if isExpanded then
@@ -25,7 +33,15 @@ viewExpansionPanelHeader { toggle, isExpanded, title } =
             [ i [ css [ Px.pa 4 ], class "material-icons" ] [ text iconName ]
             , span [ css [ Px.p2 8 4, bold, flexGrow1 ] ] [ text "Projects" ]
             ]
-
-        --        , secondaryActionAddIconBtn
+        , Maybe.map secondaryActionIconBtn secondary |> Maybe.withDefault (text "")
         ]
     ]
+
+
+secondaryActionIconBtn : { a | iconName : String, action : msg } -> Html msg
+secondaryActionIconBtn { iconName, action } =
+    button
+        [ css [ btnReset, pointer, Px.pa 4, Px.m2 4 0, flex, itemsCenter, selfEnd ]
+        , onClick action
+        ]
+        [ i [ class "material-icons" ] [ text iconName ] ]
