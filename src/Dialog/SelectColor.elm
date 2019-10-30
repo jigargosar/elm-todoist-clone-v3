1c8886056dd6df2b9a76aa18c8db59b1e392b5bc
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-30 19:52:28 +0530

    minimal elm todoist clone

--- src/Dialog/SelectColor.elm
+++ src/Dialog/SelectColor.elm
@@ -84,21 +84,24 @@ view { toMsg } model =
                 , tabindex 0
                 ]
                 (List.map viewItem allColors)
+
+        viewSelectInput =
+            div
+                (css [ boAll, boColor Theme.borderGray ]
+                    :: (if model.open then
+                            []
+
+                        else
+                            [ tabindex 0
+                            , Key.preventDefaultOnKeyDown [ Key.enterOrSpace ( Open, True ) ]
+                            ]
+                       )
+                )
+                [ viewItem model.color ]
     in
     div
         [ css [ relative, lh 1.5 ] ]
-        [ div
-            (css [ boAll, boColor Theme.borderGray ]
-                :: (if model.open then
-                        []
-
-                    else
-                        [ tabindex 0
-                        , Key.preventDefaultOnKeyDown [ Key.enterOrSpace ( Open, True ) ]
-                        ]
-                   )
-            )
-            [ viewItem model.color ]
+        [ viewSelectInput
         , viewIf model.open viewPopup
         ]
         |> H.map toMsg
