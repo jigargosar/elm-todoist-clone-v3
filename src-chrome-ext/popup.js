2e4fa7935a12631bcbfbc1cc99fabb6d71576435
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-02 13:54:47 +0530

    playing with recently closed, chrome extension

--- src-chrome-ext/popup.js
+++ src-chrome-ext/popup.js
@@ -1,9 +1,24 @@
 getRecentlyClosed = () =>
-  new Promise(resolve => chrome.sessions.getRecentelyClosed(resolve))
+  new Promise(resolve => chrome.sessions.getRecentlyClosed(resolve))
 
 async function main() {
   const sessions = await getRecentlyClosed()
   console.log('sessions', sessions)
+
+  sessions.forEach(session => {
+    const tab = session.tab
+    const window = session.window
+    if (tab) {
+      logTab(tab)
+    }
+    if (window) {
+      // console.log("window.tabs", window.tabs)
+      window.tabs.forEach(logTab)
+    }
+  })
 }
 
+function logTab({ title, url }) {
+  console.log('tab', { title, url })
+}
 main().catch(console.error)
