ee0f1779f4c73fda788507317792dcef728df4e9
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-03 12:02:33 +0530

    minimal elm todoist clone

--- src/UI/IconButton.elm
+++ src/UI/IconButton.elm
@@ -1,10 +1,12 @@
 module UI.IconButton exposing (..)
 
 import Html.Styled exposing (Html, button)
+import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events exposing (onClick)
+import Styles exposing (..)
 import UI.Icon as Icon exposing (Icon)
 
 
 view : Icon -> msg -> Html msg
 view icon action =
-    button [ onClick action ] [ Icon.view icon ]
+    button [ css [ btnReset ], onClick action ] [ Icon.view icon ]
