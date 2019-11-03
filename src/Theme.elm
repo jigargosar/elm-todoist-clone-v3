d698854b675800fcaec985cfbdd87bc51285f7b8
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-03 12:23:08 +0530

    minimal elm todoist clone

--- src/Theme.elm
+++ src/Theme.elm
@@ -1,7 +1,14 @@
 module Theme exposing (..)
 
+import Css exposing (Color)
 import Styles
 
 
+borderGray : Color
 borderGray =
     Styles.grayL 0.8
+
+
+hoverGray : Color
+hoverGray =
+    Styles.grayL 0.9
--- src/UI/IconButton.elm
+++ src/UI/IconButton.elm
@@ -5,9 +5,21 @@ import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events exposing (onClick)
 import Px
 import Styles exposing (..)
+import Theme
 import UI.Icon as Icon exposing (Icon)
 
 
 view : Icon -> msg -> Html msg
 view icon action =
-    button [ css [ btnReset, pointer, Px.ma 4 ], onClick action ] [ Icon.view icon ]
+    button
+        [ css
+            [ btnReset
+            , pointer
+            , Px.ma 4
+            , hover
+                [ bg Theme.hoverGray
+                ]
+            ]
+        , onClick action
+        ]
+        [ Icon.view icon ]
