6b80cc9a3c1825fd3d47a9eafdc9a79150a14dbd
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-05 03:46:21 +0530

    minimal elm todoist clone

--- src/Style/Elements.elm
+++ src/Style/Elements.elm
@@ -1,15 +1,19 @@
 module Style.Elements exposing (..)
 
+import Css exposing (Style)
 import Style.Elements.Button as Button
 
 
+btn : Style
 btn =
     Button.basic
 
 
+primaryBtn : Style
 primaryBtn =
     Button.primary
 
 
+linkBtn : Style
 linkBtn =
     Button.link
