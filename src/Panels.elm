1d8246dafd55baf89b73a702f9a50acc3af0127b
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-05 19:46:33 +0530

    minimal elm todoist clone

---
+++ src/Panels.elm
@@ -0,0 +1,18 @@
+module Panels exposing (filterIcon, labelIcon, projectIcon)
+
+import UI.Icon as Icon
+
+
+labelIcon : Icon.Icon
+labelIcon =
+    Icon.Label
+
+
+projectIcon : Icon.Icon
+projectIcon =
+    Icon.Folder
+
+
+filterIcon : Icon.Icon
+filterIcon =
+    Icon.FilterList
