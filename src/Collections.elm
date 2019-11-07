57ccde3453942f23d65b8ab2de15d306e02f0942
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-07 16:33:49 +0530

    cleanup

--- src/Collections.elm
+++ src/Collections.elm
@@ -1,9 +1,13 @@
 module Collections exposing (..)
 
+import Basics.More exposing (apply)
 import FilterCollection exposing (FilterCollection)
+import Json.Decode as JD
 import Json.Encode exposing (Value)
 import LabelCollection exposing (LabelCollection)
 import ProjectCollection exposing (ProjectCollection)
+import Result.Extra
+import Return
 import TodoDict exposing (TodoDict)
 
 
@@ -23,3 +27,67 @@ type alias Flags a =
         , labelList : Value
         , filterList : Value
     }
+
+
+mapTodoDict func model =
+    { model | todoDict = func model.todoDict }
+
+
+mapProjectCollection func model =
+    { model | projectCollection = func model.projectCollection }
+
+
+mapLabelCollection func model =
+    { model | labelCollection = func model.labelCollection }
+
+
+mapFilterCollection func model =
+    { model | filterCollection = func model.filterCollection }
+
+
+initCollections : Flags x -> Collections a -> ( Collections a, List JD.Error )
+initCollections flags model =
+    let
+        todoDictResult : Result JD.Error (Collections a -> Collections a)
+        todoDictResult =
+            TodoDict.fromEncodedList flags.todoList
+                |> Result.map (always >> mapTodoDict)
+
+        projectCollectionResult : Result JD.Error (Collections a -> Collections a)
+        projectCollectionResult =
+            ProjectCollection.fromEncodedList flags.projectList
+                |> Result.map (always >> mapProjectCollection)
+
+        labelCollectionResult : Result JD.Error (Collections a -> Collections a)
+        labelCollectionResult =
+            LabelCollection.fromEncodedList flags.labelList
+                |> Result.map (always >> mapLabelCollection)
+
+        filterCollectionResult : Result JD.Error (Collections a -> Collections a)
+        filterCollectionResult =
+            FilterCollection.fromEncodedList flags.filterList
+                |> Result.map (always >> mapFilterCollection)
+
+        results : List (Result JD.Error (Collections a -> Collections a))
+        results =
+            [ todoDictResult
+            , projectCollectionResult
+            , labelCollectionResult
+            , filterCollectionResult
+            ]
+
+        foo =
+            Result.Extra.unpack (Tuple.mapSecond (::))
+                (Tuple.mapFirst identity)
+    in
+    results
+        |> List.foldl
+            (\result ->
+                case result of
+                    Ok func ->
+                        Tuple.mapFirst func
+
+                    Err error ->
+                        Tuple.mapSecond ((::) error)
+            )
+            ( model, [] )
