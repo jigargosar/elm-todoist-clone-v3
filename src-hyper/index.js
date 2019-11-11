149f14b7ee978e5a340800a724d51cf5e3073bd7
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-11 15:01:29 +0530

    try hyper

--- src-hyper/index.js
+++ src-hyper/index.js
@@ -1,71 +1,15 @@
-/*
 import { h, app } from 'hyperapp'
+
+const INC = state => state + 1
+const DEC = state => state + 1
 
 app({
   init: 0,
   view: state =>
     h('main', {}, [
       h('h1', {}, state),
-      h('button', { onClick: state => state - 1 }, '-'),
-      h('button', { onClick: state => state + 1 }, '+'),
+      h('button', { onClick: INC }, '-'),
+      h('button', { onClick: DEC }, '+'),
     ]),
-  node: document.getElementById('app'),
-})
-*/
-
-import { h, app } from 'hyperapp'
-import picostyle from 'picostyle'
-const React = h
-
-const ps = picostyle(h)
-
-const state = {
-  text: 'Picostyle',
-}
-
-const actions = {}
-
-const view = state => {
-  // const keyColor = '#f07'
-  const keyColor = '#000'
-
-  const Text = ps('a')({
-    fontSize: '64px',
-    cursor: 'pointer',
-    color: '#fff',
-    padding: '0.4em',
-    transition: 'all .2s ease-in-out',
-    textDecoration: 'none',
-    ':hover': {
-      transform: 'scale(1.3)',
-    },
-    '@media (max-width: 450px)': {
-      fontSize: '32px',
-    },
-  })
-
-  const Wrapper = ps('div')({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: keyColor,
-  })
-  return h('main', {}, [
-    Wrapper(
-      {},
-      Text(
-        { href: 'https://github.com/morishitter/picostyle' },
-        state.text,
-      ),
-    ),
-  ])
-}
-
-app({
-  init: state,
-  actions,
-  view,
   node: document.getElementById('app'),
 })
