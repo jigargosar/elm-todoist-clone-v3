e38f36996f5d13dc434a13232c7200e761e5935d
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-11 15:13:49 +0530

    try hyper

--- src-hyper/index.js
+++ src-hyper/index.js
@@ -1,15 +1,24 @@
 import { h, app } from 'hyperapp'
 
+import validate from 'aproba'
+
 const INC = state => state + 1
 const DEC = state => state + 1
+
+const div = function(props = {}, children = []) {
+  validate('OA', arguments)
+  return h('div', props, children)
+}
+
+console.log(div)
 
 app({
   init: 0,
   view: state =>
-    h('main', {}, [
+    div({}, [
       h('h1', {}, state),
-      h('button', { onClick: INC }, '-'),
-      h('button', { onClick: DEC }, '+'),
+      h('button', { onClick: DEC }, '-'),
+      h('button', { onClick: INC }, '+'),
     ]),
   node: document.getElementById('app'),
 })
