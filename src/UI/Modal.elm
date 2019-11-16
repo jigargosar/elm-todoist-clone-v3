f8224e43b5e2ce8190b00115dce55004e59f3b6c
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-16 17:58:46 +0530

    styling

--- src/UI/Modal.elm
+++ src/UI/Modal.elm
@@ -9,13 +9,5 @@ view : List (Html msg) -> Html msg
 view content =
     div [ class "modal is-active" ]
         [ div [ css [ backgroundColor (hsla 0 0 0 0.2) ], class "modal-background" ] []
-        , div
-            [ css
-                [ width (px 300), maxWidth (pct 100)
-
-                {- , height (pct 200) -}
-                ]
-            , class "modal-content"
-            ]
-            content
+        , div [ class "modal-content" ] content
         ]
